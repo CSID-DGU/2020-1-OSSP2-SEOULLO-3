56ddb62eb90329bccdcd80ca9053f60d54e39263
Seunghwanly <seunghwanly@gmail.com>
2020-04-26 18:03:16 +0900

    added color

--- Seoullo/app/src/main/java/com/seoullo/seoullotour/Map/MapActivity.java
+++ Seoullo/app/src/main/java/com/seoullo/seoullotour/Map/MapActivity.java
@@ -1,23 +1,84 @@
 package com.seoullo.seoullotour.Map;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.seoullo.seoullotour.R;
+import com.seoullo.seoullotour.Utils.BottomNavigationViewHelper;
+import com.seoullo.seoullotour.Utils.SectionsPagerAdapter;
 
 public class MapActivity extends AppCompatActivity {
     private static final String TAG = "MapActivity";
     public static final int ACTIVITY_NUM = 3;
+    private Context mContext = MapActivity.this;
+    //widget
+    private ViewPager mViewPager;
+    private FrameLayout mFrameLayout;
+    private RelativeLayout mRelativeLayout;
+    private RelativeLayout topTab;
 
     //TODO: link to fragment
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting.");
+        mFrameLayout = (FrameLayout) findViewById(R.id.container);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relLayoutParent);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager_container);
+        topTab = (RelativeLayout) findViewById(R.id.relLayout1);
+        topTab.setVisibility(View.INVISIBLE);
+
+        setupBottomNavigationView();
+        setupViewPager();
+    }
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, this,bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
+    }
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MapFragment());
+        mViewPager.setAdapter(adapter);
+    }
+    public void hideLayout(){
+        Log.d(TAG, "hideLayout: hiding layout");
+        mRelativeLayout.setVisibility(View.GONE);
+        mFrameLayout.setVisibility(View.VISIBLE);
+    }
+
+
+    public void showLayout(){
+        Log.d(TAG, "hideLayout: showing layout");
+        mRelativeLayout.setVisibility(View.VISIBLE);
+        mFrameLayout.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if(mFrameLayout.getVisibility() == View.VISIBLE){
+            showLayout();
+        }
     }
 }
