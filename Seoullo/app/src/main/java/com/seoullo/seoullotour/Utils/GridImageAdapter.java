0ae168d480d0b42a7804c6d6d3718d36335ee835
jonghapark <59429710+jonghapark@users.noreply.github.com>
2020-05-27 19:40:36 +0900

    ProfileProgress

--- Seoullo/app/src/main/java/com/seoullo/seoullotour/Utils/GridImageAdapter.java
+++ Seoullo/app/src/main/java/com/seoullo/seoullotour/Utils/GridImageAdapter.java
@@ -2,6 +2,7 @@ package com.seoullo.seoullotour.Utils;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +13,14 @@ import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -22,11 +28,12 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import com.seoullo.seoullotour.Models.Photo;
+import com.seoullo.seoullotour.Profile.ProfileFragment;
 import com.seoullo.seoullotour.R;
 
 import java.util.ArrayList;
 
-public class GridImageAdapter extends ArrayAdapter<String> {
+public class GridImageAdapter extends ArrayAdapter<String>  {
 
     private Context mContext;
     private LayoutInflater mInflater;
@@ -38,6 +45,9 @@ public class GridImageAdapter extends ArrayAdapter<String> {
     private ArrayList<String> imgURLs;
     private ArrayList<Photo> photos = new ArrayList<>();
     public RequestManager mRequestManager;
+    private  ProfileFragment profileFragment;
+    private int loadSize;
+
 
     public GridImageAdapter(Context context, int layoutResource, String append, ArrayList<String> imgURLs , String uid, int flag) {
         super(context, layoutResource, imgURLs);
@@ -49,10 +59,12 @@ public class GridImageAdapter extends ArrayAdapter<String> {
         mAppend = append;
         this.imgURLs = imgURLs;
     }
-    public GridImageAdapter(Context context, int layoutResource, String append, ArrayList<String> imgURLs , String uid, int flag,  ArrayList<Photo> photos , RequestManager requestManager) {
+    public GridImageAdapter(Context context, int layoutResource, String append, ArrayList<String> imgURLs , String uid, int flag,
+                            ArrayList<Photo> photos , RequestManager requestManager,ProfileFragment profileFragment) {
         super(context, layoutResource, imgURLs);
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mRequestManager =requestManager;
+        this.profileFragment = profileFragment;
         mContext = context;
         this.gridFlag = flag;
         this.uid = uid;
@@ -60,6 +72,7 @@ public class GridImageAdapter extends ArrayAdapter<String> {
         mAppend = append;
         this.imgURLs = imgURLs;
         this.photos = photos;
+        loadSize = photos.size();
     }
 
 
@@ -69,7 +82,7 @@ public class GridImageAdapter extends ArrayAdapter<String> {
      */
     private static class ViewHolder {
         com.seoullo.seoullotour.Utils.SquareImageView image;
-        ProgressBar mProgressBar;
+        //ProgressBar mProgressBar;
     }
 
     /**
@@ -87,7 +100,7 @@ public class GridImageAdapter extends ArrayAdapter<String> {
         if(convertView == null) {
             convertView = mInflater.inflate(layoutResource, parent, false);
             holder = new ViewHolder();
-            holder.mProgressBar = (ProgressBar) convertView.findViewById(R.id.gridImageProgressbar);
+            //holder.mProgressBar = (ProgressBar) convertView.findViewById(R.id.gridImageProgressbar);
             holder.image = (com.seoullo.seoullotour.Utils.SquareImageView) convertView.findViewById(R.id.gridImageView);
 
             convertView.setTag(holder);
@@ -106,8 +119,40 @@ public class GridImageAdapter extends ArrayAdapter<String> {
                         .addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
                             public void onSuccess(Uri uri) {
-                                mRequestManager.load(uri).into(holder.image);
-                                holder.mProgressBar.setVisibility(View.GONE);
+
+                                System.out.println(loadSize+"::loadSize");
+                                //loadCount++;
+                                mRequestManager.load(uri)
+                                        .listener(
+                                new RequestListener<Drawable>() {
+                                    @Override
+                                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                        //holder.mProgressBar.setVisibility(View.GONE);
+
+                                        loadSize--;
+                                        if(loadSize == 0){
+                                            System.out.println("progress실험중");
+                                            profileFragment.goneProgress();
+                                        }
+                                        return false;
+                                    }
+
+                                    @Override
+                                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                        //holder.mProgressBar.setVisibility(View.GONE);
+
+                                        loadSize--;
+                                        if(loadSize == 0){
+                                            System.out.println("progress실험중");
+                                            profileFragment.goneProgress();
+                                        }
+                                        return false;
+                                    }
+                                })
+
+
+                                        .into(holder.image);
+                               // holder.mProgressBar.setVisibility(View.GONE);
 //                                Glide.with(mContext)
 //                                        .load(uri)
 //                                        .into(holder.image);
@@ -122,38 +167,60 @@ public class GridImageAdapter extends ArrayAdapter<String> {
             imageLoader.displayImage(mAppend + imgURL, holder.image, new ImageLoadingListener() {
                 @Override
                 public void onLoadingStarted(String imageUri, View view) {
-                    if (holder.mProgressBar != null) {
-                        holder.mProgressBar.setVisibility(View.VISIBLE);
-                    }
+//                    if (holder.mProgressBar != null) {
+//                        holder.mProgressBar.setVisibility(View.VISIBLE);
+                    loadSize++;
+//                    }
 
                 }
 
                 @Override
                 public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-                    if (holder.mProgressBar != null) {
-                        holder.mProgressBar.setVisibility(View.GONE);
+//                    if (holder.mProgressBar != null) {
+//                        holder.mProgressBar.setVisibility(View.GONE);
+//                    }
+
+                    loadSize--;
+                    if(loadSize == 0){
+                        System.out.println("progress실험중");
+                        profileFragment.goneProgress();
                     }
 
                 }
 
                 @Override
                 public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-                    if (holder.mProgressBar != null) {
-                        holder.mProgressBar.setVisibility(View.GONE);
+//                    if (holder.mProgressBar != null) {
+//                        holder.mProgressBar.setVisibility(View.GONE);
+//                    }
+
+                    loadSize--;
+                    if(loadSize == 0){
+                        System.out.println("progress실험중");
+                        profileFragment.goneProgress();
                     }
 
                 }
 
                 @Override
                 public void onLoadingCancelled(String imageUri, View view) {
-                    if (holder.mProgressBar != null) {
-                        holder.mProgressBar.setVisibility(View.GONE);
-                    }
+//                    if (holder.mProgressBar != null) {
+//                        holder.mProgressBar.setVisibility(View.GONE);
+//                    }
 
+                    loadSize--;
+                    if(loadSize == 0){
+                        System.out.println("progress실험중");
+                        profileFragment.goneProgress();
+                    }
                 }
             });
         }
 
+
+
         return convertView;
     }
+
+
 }
