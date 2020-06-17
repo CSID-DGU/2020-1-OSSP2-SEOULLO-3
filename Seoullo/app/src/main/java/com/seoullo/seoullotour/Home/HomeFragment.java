d8bf5a67e970e2653cd82e3137669a62c240c3f7
jonghapark <59429710+jonghapark@users.noreply.github.com>
2020-06-17 19:42:46 +0900

    nullPointerErrorFix
    그리드 부분 앱 종료문제 해결

--- Seoullo/app/src/main/java/com/seoullo/seoullotour/Home/HomeFragment.java
+++ Seoullo/app/src/main/java/com/seoullo/seoullotour/Home/HomeFragment.java
@@ -66,7 +66,6 @@ public class HomeFragment extends Fragment {
         bundle.putString(ARG_PARAM1, photoID);
         bundle.putSerializable("object", clickedPhoto);
         fragment.setArguments(bundle);
-
         return fragment;
     }
     public static Fragment newInstance(Bookmark clickedPhoto, String photoID) {
@@ -75,7 +74,6 @@ public class HomeFragment extends Fragment {
         bundle.putString(ARG_PARAM1, photoID);
         bundle.putSerializable("object", clickedPhoto);
         fragment.setArguments(bundle);
-
         return fragment;
     }
     @Override
@@ -109,33 +107,35 @@ public class HomeFragment extends Fragment {
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
 
-                            Photo photo = new Photo();
-                            Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
-                            if (!objectMap.get(getString(R.string.field_photo_id)).toString().equals(mParam))
-                                continue;
-                            photo.setCaption(objectMap.get(getString(R.string.field_caption)).toString());
-                            photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
-                            photo.setPhoto_id(objectMap.get(getString(R.string.field_photo_id)).toString());
-                            photo.setImage_name(objectMap.get("image_name").toString());
-                            photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
-                            photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
-                            photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
-                            photo.setLikeCount(Integer.parseInt(objectMap.get("likeCount").toString()));
-                            ArrayList<Comment> comments = new ArrayList<Comment>();
-                            for (DataSnapshot dSnapshot : singleSnapshot
-                                    .child(getString(R.string.field_comments)).getChildren()) {
-                                Comment comment = new Comment();
-                                comment.setUser_id(dSnapshot.getValue(Comment.class).getUser_id());
-                                comment.setComment(dSnapshot.getValue(Comment.class).getComment());
-                                comment.setDate_created(dSnapshot.getValue(Comment.class).getDate_created());
-                                comments.add(comment);
+                        if(dataSnapshot.getValue() != null) {
+                            for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                                Photo photo = new Photo();
+                                Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
+                                if (!objectMap.get(getString(R.string.field_photo_id)).toString().equals(mParam))
+                                    continue;
+                                photo.setCaption(objectMap.get(getString(R.string.field_caption)).toString());
+                                photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
+                                photo.setPhoto_id(objectMap.get(getString(R.string.field_photo_id)).toString());
+                                photo.setImage_name(objectMap.get("image_name").toString());
+                                photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
+                                photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
+                                photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
+                                photo.setLikeCount(Integer.parseInt(objectMap.get("likeCount").toString()));
+                                ArrayList<Comment> comments = new ArrayList<Comment>();
+                                for (DataSnapshot dSnapshot : singleSnapshot
+                                        .child(getString(R.string.field_comments)).getChildren()) {
+                                    Comment comment = new Comment();
+                                    comment.setUser_id(dSnapshot.getValue(Comment.class).getUser_id());
+                                    comment.setComment(dSnapshot.getValue(Comment.class).getComment());
+                                    comment.setDate_created(dSnapshot.getValue(Comment.class).getDate_created());
+                                    comments.add(comment);
+                                }
+
+                                photo.setComments(comments);
+                                mPhotos.add(photo);
+
                             }
-
-                            photo.setComments(comments);
-                            mPhotos.add(photo);
-
                         }
                     }
 
